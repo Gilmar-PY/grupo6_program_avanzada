a294d7730a596adac2531edba0a41774305fb97e
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-11-14 18:54:15 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -5,3 +5,5 @@ import pandas as pn
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
+
+df_licenciamiento_institucional = pd.read_excel(r'https://www.datosabiertos.gob.pe/sites/default/files/Licenciamiento%20Institucional_7.csv', header= 0) 
