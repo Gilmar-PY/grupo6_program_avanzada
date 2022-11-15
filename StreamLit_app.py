ecd2f95fa5ba2dc24f0dd5fbadc881d1ca3815bd
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-11-14 18:49:06 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -1,4 +1,7 @@
+import pip
+pip.main(["install", "openpyxl"])
+pip.main(["install", "pandas"])
 import pandas as pn
 import numpy as np
-import skimage
 import matplotlib.pyplot as plt
+import streamlit as st
